b3916c6a1b9d6a2135edbf444e0913a2b9416779
Petar Nikolic <pnikolic12524si@raf.rs>
2025-06-08 15:07:34 +0200

    Update configs and dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: 'automatic' })],
   server: {
     proxy: {
       '/api': 'http://localhost:3001',   // adjust if your backend port differs
